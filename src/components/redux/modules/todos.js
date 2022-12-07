bf6c39030ca03311f1ac380fa65d3162f7f0ac95
hannahcodes0 <hannahhayoungkim2@gmail.com>
2022-12-08 01:04:33 +0900

    reducers added

---
+++ src/components/redux/modules/todos.js
@@ -0,0 +1,96 @@
+// Action Value
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const TOGGLE_STATUS_TODO = "TOGGLE_STATUS_TODO";
+const GET_TODO_BY_ID = "GET_TODO_BY_ID";
+
+// Action Creator
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload,
+  };
+};
+
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload,
+  };
+};
+
+export const toggleStatusTodo = (payload) => {
+  return {
+    type: TOGGLE_STATUS_TODO,
+    payload,
+  };
+};
+
+export const getTodoById = (payload) => {
+  return {
+    type: GET_TODO_BY_ID,
+    payload,
+  };
+};
+
+// 초기 상태값
+const initialState = {
+  todos: [
+    {
+      id: "1",
+      title: "리액트",
+      body: "리액트를 배워봅시다",
+      isDone: false,
+    },
+  ],
+  todo: {
+    id: "",
+    title: "",
+    body: "",
+    isDone: false,
+  },
+};
+
+// 리듀서
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+
+    case TOGGLE_STATUS_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id === action.payload) {
+            return {
+              ...todo,
+              isDone: !todo.isDone,
+            };
+          } else {
+            return todo;
+          }
+        }),
+      };
+
+    case GET_TODO_BY_ID:
+      return {
+        ...state,
+        todo: state.todos.find((todo) => {
+          return todo.id === action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todos;
